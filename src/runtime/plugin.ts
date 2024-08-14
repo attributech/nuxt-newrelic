fd3745cb87e4604eca6c403da4e845132429efba
Lukas von Blarer <luk@attribute.ch>
2024-08-14 22:02:23 +0200

    fix: defineNitroPlugin not defined

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,10 @@
 import newrelic from 'newrelic'
 import type { H3Event } from 'h3'
+import type { NitroAppPlugin } from 'nitropack'
+
+export function defineNitroPlugin(def: NitroAppPlugin) {
+  return def
+}
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('request', (event: H3Event) => {
