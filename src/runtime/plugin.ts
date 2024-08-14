b5d22d304456a11d2129b285c11b06748c5a11d8
Lukas von Blarer <luk@attribute.ch>
2024-08-14 19:28:55 +0200

    fix: Typing hook arguments

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,10 +1,11 @@
 import newrelic from 'newrelic'
+import type { H3Event } from 'h3'
 
 export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('request', (event) => {
+  nitroApp.hooks.hook('request', (event: H3Event) => {
     newrelic.setTransactionName(event.path)
   })
-  nitroApp.hooks.hook('error', (error, context) => {
+  nitroApp.hooks.hook('error', (error: Error) => {
     newrelic.noticeError(error)
   })
 })
