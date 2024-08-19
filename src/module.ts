d5ab9826868633855c3ae10c0c10b971103066fa
Lukas von Blarer <luk@attribute.ch>
2024-08-19 12:59:39 +0200

    Only tack certain types of nuxt errors #1

--- src/module.ts
+++ src/module.ts
@@ -1,14 +1,24 @@
 import { defineNuxtModule, createResolver, addServerPlugin } from '@nuxt/kit'
+import { defu } from 'defu'
+import type { newrelicModuleOptions } from './runtime/types'
 
-export interface ModuleOptions {}
+export const defaultOptions: newrelicModuleOptions = {
+  errorClasses: [5],
+}
 
-export default defineNuxtModule<ModuleOptions>({
+export default defineNuxtModule<newrelicModuleOptions>({
   meta: {
     name: 'nuxt-newrelic',
     configKey: 'newrelic',
   },
-  defaults: {},
-  setup(_options, _nuxt) {
+  defaults: defaultOptions,
+  setup(passedOptions, nuxt) {
+    const options = defu(
+      {},
+      passedOptions,
+      defaultOptions,
+    ) as newrelicModuleOptions
+    nuxt.options.runtimeConfig.newrelic = options
     const resolver = createResolver(import.meta.url)
     addServerPlugin(resolver.resolve('./runtime/plugin'))
   },
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,16 +1,41 @@
 import newrelic from 'newrelic'
 import type { H3Event } from 'h3'
 import type { NitroAppPlugin } from 'nitropack'
+import type { newrelicModuleOptions } from './types'
+import { useRuntimeConfig } from '#imports'
+import type { NuxtError } from '#app'
+import {} from '@nuxt/kit'
 
+// @todo: Stubbing defineNitroPlugin. Maybe this could be solved differently?
 export function defineNitroPlugin(def: NitroAppPlugin) {
   return def
 }
 
 export default defineNitroPlugin((nitroApp) => {
+  const runtimeConfig = useRuntimeConfig().newrelic as newrelicModuleOptions
   nitroApp.hooks.hook('request', (event: H3Event) => {
+    // Name the transaction to the value the request's path
     newrelic.setTransactionName(event.path)
   })
   nitroApp.hooks.hook('error', (error: Error) => {
+    // Check if we have a nuxt error that should be ignored
+    if (isNuxtError(error)) {
+      const nuxtError: NuxtError = error
+      const cause = nuxtError?.cause
+      if (typeof cause === 'object' && cause !== null && 'statusCode' in cause) {
+        const statusCode = cause.statusCode
+        const errorClass = Number(String(statusCode)[0])
+        if (!runtimeConfig.errorClasses.includes(errorClass)) {
+          return
+        }
+      }
+    }
+    // Send error to newrelic agent
     newrelic.noticeError(error)
   })
 })
+
+// @todo: Stubbing isNuxtError, since somehow this can't be imported.
+const isNuxtError = <DataT = unknown>(
+  error?: string | object,
+): error is NuxtError<DataT> => !!error && typeof error === 'object' && '__nuxt_error' in error
